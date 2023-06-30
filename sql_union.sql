bb43631c7ac2a166bae846754e05c4b46cb26779
abhabajpai <105857736+abhabajpai@users.noreply.github.com>
2023-06-30 07:39:21 +0530

    Add files via upload

---
+++ sql_union.sql
@@ -0,0 +1,16 @@
+SELECT
+customer_id,
+first_name,
+points,
+'Bronze' AS TYPE
+FROM customers
+WHERE points < 2000
+UNION
+SELECT
+customer_id,
+first_name,
+points,
+'Silver' AS TYPE
+FROM customers
+WHERE points BETWEEN 2000 AND 3000
+
